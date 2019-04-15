3dd41b8730be65d13660c18af8e35fb9b2cb782f
gilles orsini <gilles.orsini@gmail.com>
2019-04-15 23:09:34 +0200

    rajout extracteur annonce leboncoin

---
+++ extractors/leboncoin.js
@@ -0,0 +1,6 @@
+result.title = $("div[data-qa-id='adview_spotlight_container'] div[data-qa-id='adview_title'] h1").text();
+result.prix = $("div[data-qa-id='adview_spotlight_container'] div[data-qa-id='adview_price'] > div > span").text();
+result.date = $("div[data-qa-id='adview_spotlight_container'] div[data-qa-id='adview_date']").text();
+result.text = $(".content-CxPmi").text();
+result.location = $("div[data-qa-id='adview_location_informations'] span").text();
+result.vendeur = $("div[data-qa-id='adview_contact_container'] a.trackable:eq(1)").text();
