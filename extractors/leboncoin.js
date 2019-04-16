f6ea7320823aba30dc0a5d08f7b7c2011e3c1671
gilles orsini <gilles.orsini@gmail.com>
2019-04-16 22:34:02 +0200

    rajout extract info processeur, RAM

--- extractors/leboncoin.js
+++ extractors/leboncoin.js
@@ -1,6 +1,11 @@
+text = $(".content-CxPmi").text();
 result.title = $("div[data-qa-id='adview_spotlight_container'] div[data-qa-id='adview_title'] h1").text();
 result.prix = $("div[data-qa-id='adview_spotlight_container'] div[data-qa-id='adview_price'] > div > span").text();
 result.date = $("div[data-qa-id='adview_spotlight_container'] div[data-qa-id='adview_date']").text();
-result.text = $(".content-CxPmi").text();
+result.text = text;
+result.processeur = text.match(/i[357]/)[0];
+result.ecran = text.match(/1\d([,.]\d)?/)[0];
+result.memoire = text.match(/(8|16|32|64)/)[0];
 result.location = $("div[data-qa-id='adview_location_informations'] span").text();
 result.vendeur = $("div[data-qa-id='adview_contact_container'] a.trackable:eq(1)").text();
+result.url = window.location.href;
